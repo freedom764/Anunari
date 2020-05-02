5634e650df2890eb21f23420507d944b236b835f
freedom764 <gkfigfo@gmail.com>
2020-05-02 20:00:40 +0600

    efef
    efe

---
+++ Dodle anunari/Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+   
+   
+  
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        if (target.position.y > transform.position.y) {
+            Vector3 newPos  = new Vector3(0f, target.position.y, transform.position.z);
+            transform.position = newPos;
+                
+        }
+    }
+}
---
+++ Dodle anunari/Assets/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+
+    public float speed = 8f;
+
+    Rigidbody2D rb;
+
+    public float movement = 0f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+   
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal") * speed;
+
+    }
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = movement;
+        rb.velocity = velocity;
+    }
+}
